03a11f4e52bbfe4cffec474deb3fcbc7e865c440
Dayvson Silva <userddssilva@gmail.com>
2024-02-09 17:55:34 -0400

    Rename fragment and add the recycleview

--- app/src/main/java/com/financialchecker/ui/expense/ExpenseFragment.kt
+++ app/src/main/java/com/financialchecker/ui/expense/ExpenseFragment.kt
@@ -1,4 +1,4 @@
-package com.financialchecker.ui.home
+package com.financialchecker.ui.expense
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.financialchecker.databinding.FragmentHomeBinding
+import com.financialchecker.databinding.FragmentExpenseBinding
 
-class HomeFragment : Fragment() {
+class ExpenseFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentExpenseBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,16 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val expenseViewModel =
+            ViewModelProvider(this).get(ExpenseViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentExpenseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        expenseViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
--- app/src/main/java/com/financialchecker/ui/expense/ExpenseViewModel.kt
+++ app/src/main/java/com/financialchecker/ui/expense/ExpenseViewModel.kt
@@ -1,10 +1,10 @@
-package com.financialchecker.ui.home
+package com.financialchecker.ui.expense
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ExpenseViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
